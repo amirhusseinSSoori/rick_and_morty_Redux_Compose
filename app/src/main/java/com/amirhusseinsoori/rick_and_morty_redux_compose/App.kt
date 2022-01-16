2dee4759c5886f31f2a23561c49793658720558d
Amirhussein soori <amirhusseinsppro75@gmail.com>
2022-01-16 20:52:05 +0330

    initial base libraries

---
+++ app/src/main/java/com/amirhusseinsoori/rick_and_morty_redux_compose/App.kt
@@ -0,0 +1,7 @@
+package com.amirhusseinsoori.rick_and_morty_redux_compose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App :Application()
--- app/src/main/java/com/amirhusseinsoori/rick_and_morty_redux_compose/MainActivity.kt
+++ app/src/main/java/com/amirhusseinsoori/rick_and_morty_redux_compose/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.amirhusseinsoori.rick_and_morty_redux_compose.ui.theme.Rick_and_morty_Redux_ComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
